634aaffcd3529621375c8fc5a59c8b374ebdb768
Vipul Popat <vipul.popat2@mail.dcu.ie>
2023-02-26 22:15:43 +0000

    First cut

---
+++ contracts/EventTickets.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+uint256 constant TOTAL_EVENT_TICKETS = 10;
+
+contract EventTickets {
+  address public owner = msg.sender;
+
+  struct EventTicket {
+    uint256 price;
+    address owner;
+  }
+
+  EventTicket[TOTAL_EVENT_TICKETS] public eventTickets;
+
+  constructor() {
+    for (uint256 i = 0; i < TOTAL_EVENT_TICKETS; i++) {
+      eventTickets[i].price = 1e17; // 0.1 ETH
+      eventTickets[i].owner = address(0x0);
+    }
+  }
+
+  function purchaseEventTicket(uint256 _index) external payable {
+    require(_index < TOTAL_EVENT_TICKETS && _index >= 0);
+    require(eventTickets[_index].owner == address(0x0));
+    require(msg.value >= eventTickets[_index].price);
+    eventTickets[_index].owner = msg.sender;
+  }
+}
---
+++ test/EventTickets.js
@@ -0,0 +1,24 @@
+const EventTickets = artifacts.require('EventTickets');
+const assert = require('assert');
+
+contract('EventTickets', (accounts) => {
+  const BUYER_ACCOUNT = accounts[1];
+  const EVENT_TICKET_ID = 0;
+
+  it('should allower a user to buy a event ticket', async () => {
+    const instance = await EventTickets.deployed();
+    const originalTicket = await instance.eventTickets(
+      EVENT_TICKET_ID
+    );
+    await instance.purchaseEventTicket(EVENT_TICKET_ID, {
+      from: BUYER_ACCOUNT,
+      value: originalTicket.price,
+    });
+    const updatedEventTicket = await instance.eventTickets(EVENT_TICKET_ID);
+    assert.equal(
+      updatedEventTicket.owner,
+      BUYER_ACCOUNT,
+      'the buyer should now own this event ticket'
+    );
+  });
+});
